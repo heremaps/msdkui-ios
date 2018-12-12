659ebc7a4c18313159aef5c1138b57f6ab9452b7
Otavio Cordeiro <41054361+otaviohere@users.noreply.github.com>
2018-12-12 15:56:12 +0100

    MSDKUI-1672: Fix Speed Unit for UK
    UK uses a mix of Imperial and Metric System. In Swift, Locale has a
    property called .usesMetricSystem which is a boolean. For UK, it returns
    true.
    
    In Objective-C, Local has 3 possible values for metric system, US, UK,
    Metric. This commit uses the Objective-C bridge to check the Locale's
    measurement system.
    
    Signed-off-by: Otavio Cordeiro <41054361+otaviohere@users.noreply.github.com>

---
+++ MSDKUI_Tests/ExtensionLocaleTests.swift
@@ -0,0 +1,46 @@
+//
+// Copyright (C) 2017-2018 HERE Europe B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+@testable import MSDKUI
+import XCTest
+
+final class ExtensionLocaleTests: XCTestCase {
+
+    /// Tests the `.usesKilometersPerHour` behavior for Germany.
+    func testUsesKilometersPerHourForGermany() {
+        XCTAssertTrue(Locale(identifier: "de_DE").usesKilometersPerHour, "It uses km/h for speed.")
+    }
+
+    /// Tests the `.usesKilometersPerHour` behavior for the US.
+    func testUsesKilometersPerHourForUS() {
+        XCTAssertFalse(Locale(identifier: "en_US").usesKilometersPerHour, "It doesn't use km/h for speed.")
+    }
+
+    /// Tests the `.usesKilometersPerHour` behavior for UK.
+    func testUsesKilometersPerHourForUK() {
+        XCTAssertFalse(Locale(identifier: "en_GB").usesKilometersPerHour, "It doesn't use km/h for speed.")
+    }
+
+    /// Tests the `.usesKilometersPerHour` behavior for Brazil.
+    func testUsesKilometersPerHourForBrazil() {
+        XCTAssertTrue(Locale(identifier: "pt_BR").usesKilometersPerHour, "It uses km/h for speed.")
+    }
+
+    /// Tests the `.usesKilometersPerHour` behavior for an invalid Locale.
+    func testUsesKilometersPerHourForInvalidLocale() {
+        XCTAssertTrue(Locale(identifier: "BADBEEF").usesKilometersPerHour, "It uses km/h for speed.")
+    }
+}
